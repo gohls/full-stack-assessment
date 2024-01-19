6e0bfeeb71a5514b0755884973accfe3d06cadd2
Simon Gohl <sagohl@gmail.com>
2024-01-19 14:03:00 -0800

    update DB

--- client-dashboard/ClientDashboardAPI/sql-scripts/setup.sql
+++ client-dashboard/ClientDashboardAPI/sql-scripts/setup.sql
@@ -1,4 +1,20 @@
-CREATE TABLE ClientDashboardDB.Clients(
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-FirstName VARCHAR(255) NOT NULL,
-LastName VARCHAR(255) NOT NULL);
+CREATE TABLE ClientDashboardDB.Clients (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Email VARCHAR(255),
+    LastUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ClientDashboardDB.AlternativePhoneNumbers (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    PhoneNumber VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE ClientDashboardDB.ClientAlternativePhoneNumbers (
+    ClientID INT UNSIGNED,
+    AlternativePhoneNumberID INT UNSIGNED,
+    PRIMARY KEY (ClientID, AlternativePhoneNumberID),
+    FOREIGN KEY (ClientID) REFERENCES ClientDashboardDB.Clients(id),
+    FOREIGN KEY (AlternativePhoneNumberID) REFERENCES ClientDashboardDB.AlternativePhoneNumbers(id)
+);
