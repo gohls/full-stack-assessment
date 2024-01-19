2136a50988b7009262d0a65a1b4662274c005c35
Simon Gohl <sagohl@gmail.com>
2024-01-19 14:11:00 -0800

    update Client DTO

--- client-dashboard/ClientDashboardAPI/DTO/Client.cs
+++ client-dashboard/ClientDashboardAPI/DTO/Client.cs
@@ -5,5 +5,8 @@ namespace ClientDashboardAPI.DTOs
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public bool Archive { get; set; }
     }
 }
