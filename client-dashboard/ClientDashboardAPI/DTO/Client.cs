034454ca6933072d70dee95fc270102692e13477
Simon Gohl <sagohl@gmail.com>
2024-01-19 16:02:49 -0800

    update: add additonal fields

--- client-dashboard/ClientDashboardAPI/DTO/Client.cs
+++ client-dashboard/ClientDashboardAPI/DTO/Client.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace ClientDashboardAPI.DTOs
 {
     public class Client
@@ -5,8 +8,10 @@ namespace ClientDashboardAPI.DTOs
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PrimaryPhoneNumber { get; set; }
         public string Email { get; set; }
         public DateTime LastUpdated { get; set; }
         public bool Archive { get; set; }
+        public List<string> AlternativeNumbers { get; set; }
     }
 }
